240db264901a6ee77bf8c59ae0026f5cd3e5cf0b
Prayas Yadav <prayasyadav248@gmail.com>
2025-12-30 20:01:18 +0530

    Logger configurations

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import logger from './config/logger.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
+  logger.info('Hello from the dev(bablu)!')
   res.status(200).send('Hello, World!');
 });
 
