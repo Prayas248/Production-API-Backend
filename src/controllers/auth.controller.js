53563fba46571fb50435910a190e205fadac96de
Prayas Yadav <prayasyadav248@gmail.com>
2026-01-13 04:39:21 +0530

    signin controller and routes created

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,11 @@
 import logger from '../config/logger.js';
-import { signupSchema } from '../validations/auth.validation.js';
+import { signupSchema, signinSchema } from '../validations/auth.validation.js';
 import { formatValidationError } from '../utils/format.js';
-import { createUser } from '../services/auth.service.js';
+import { createUser, authenticateUser } from '../services/auth.service.js';
 import { jwttoken } from '../utils/jwt.js';
 import { cookies } from '../utils/cookies.js';
 
+//Signup controller
 export const signup = async (req, res, next) => {
     try{
         const validationResult = signupSchema.safeParse(req.body);
@@ -39,7 +40,43 @@ export const signup = async (req, res, next) => {
         if(e.message === 'User with this email already exists'){
             return res.status(409).json({ error: 'Email already exists' });
         }
-        
+        next(e);
+    }   
+    
+}
+
+
+
+//Signin controller
+export const signin = async (req, res, next) => {
+    try{
+        const validationResult = signinSchema.safeParse(req.body);
+        if(!validationResult.success){
+            return res.status(400).json({ error: 'Validation failed' ,
+                 details: formatValidationError(validationResult.error)});
+        }
+
+        const { email, password } = validationResult.data;
+        const user = await authenticateUser(email, password);
+
+        const token = jwttoken.sign({ id: user.id, email: user.email, role: user.role });
+        cookies.set(res,'token',token);
+
+
+        logger.info('User signed in successfully', { email });
+        return  res.status(200).json({
+            message: 'User signed in successfully',
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            }
+        });
+
+    }
+    catch(e){
+        logger.error('Error during user signin', e);
         next(e);
     }
 }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,6 +2,7 @@ import logger from '../config/logger.js';
 import bcrypt from 'bcrypt';
 import { pool } from '../config/database.js';
 
+// password hashing function
 export const hashedPassword = async (password) => {
     try{
         return await bcrypt.hash(password,10);
@@ -12,7 +13,23 @@ export const hashedPassword = async (password) => {
     }
 }
 
+// password comparison function
+export const comparePassword = async (plainPassword, hashedPassword) => {
+    try {
+        if (!plainPassword || !hashedPassword) {
+        throw new Error("Password and hash are required");
+        }
 
+        return await bcrypt.compare(plainPassword, hashedPassword);
+    } 
+    catch (e) {
+        logger.error("Failed to compare password", e);
+        throw new Error("Failed to compare password");
+    }
+};
+
+
+// user creation function
 export const createUser = async (name, email, password, role = 'user') => {
     try{
         const checkUserQuery = `
@@ -31,9 +48,9 @@ export const createUser = async (name, email, password, role = 'user') => {
         const passwordHash =  await hashedPassword(password);
 
         const insertUserQuery = `
-        INSERT INTO users (name, email, password, role)
-        VALUES ($1, $2, $3, $4)
-        RETURNING id, name, email, role, created_at, updated_at
+            INSERT INTO users (name, email, password, role)
+            VALUES ($1, $2, $3, $4)
+            RETURNING id, name, email, role, created_at, updated_at
         `;
         const values = [name, email, passwordHash, role];
         const result =  await pool.query(insertUserQuery, values);
@@ -46,5 +63,44 @@ export const createUser = async (name, email, password, role = 'user') => {
     catch(e){
         logger.error('Error creating user', e);
         throw new Error('Error creating user');
+    }
+}
+
+// authenticate user function
+export const authenticateUser = async (email, password) => {
+    try{
+        const checkUserQuery = `
+        SELECT *
+        FROM users
+        WHERE email = $1
+        LIMIT 1
+        `;
+        const { rows } = await pool.query(checkUserQuery, [email]);
+
+        // if no entry found
+        if(rows.length === 0){
+            throw new Error("User does not exist");
+        }
+
+        // user is found
+        logger.info('User found by email', { email });
+
+        // check password matching
+        const passwordHash = rows[0].password;
+        const isPasswordValid = await comparePassword(password, passwordHash);
+
+        // invalid password
+        if(!isPasswordValid){
+            throw new Error("Invalid password");
+        }
+
+        // valid user
+        logger.info('Password matched for user', { email });
+        return rows[0];
+
+    }
+    catch(e){
+        logger.error('Error finding user by email', e);
+        throw new Error('Error finding user by email');
     }
 }
