8840c655eb9bc86de1f8f195f158b77a12bd3161
Prayas Yadav <prayasyadav248@gmail.com>
2026-01-16 06:32:26 +0530

    added security middleware

---
+++ src/middleware/security.middleware.js
@@ -0,0 +1,62 @@
+import { slidingWindow } from '@arcjet/node';
+import aj from '../config/arcjet.js';
+import logger from '../config/logger.js';
+
+ export const securityMiddleware = async (req, res, next) => {
+    try{
+        const role = req.user?.role || 'guest';
+
+        let limit;
+        let message;
+
+        switch(role){
+            case 'admin':
+                limit = 20
+                message = 'Admin rate limit exceeded (20 per minute)'
+                break;
+            case 'user':
+                limit = 10
+                message = 'Admin rate limit exceeded (10 per minute)'
+                break;
+            case 'guest':
+                limit = 5
+                message = 'Admin rate limit exceeded (5 per minute)'
+                break;
+            
+        }
+        const client = aj.withRule(slidingWindow({
+            mode:'LIVE',
+            interval:'1m',
+            max:limit,
+            name:`${role}-rate-limit`
+        }));
+        
+        const decision = await client.protect(req);
+
+        if(decision.isDenied() && decision.reason.isBot()){
+            logger.warn('Bot request blocker', { ip: req.ip, userAgent:req.get('User-Agent'), path:req.path });
+            
+            return res.status(403).json({ error: 'Forbidden', message: 'Bot access denied' });
+        }
+
+        if(decision.isDenied() && decision.reason.isShield()){
+            logger.warn('Shield request blocker', { ip: req.ip, userAgent:req.get('User-Agent'), path:req.path, method:req.method });
+            
+            return res.status(403).json({ error: 'Forbidden', message: 'Request blocked by security policy' });
+        }
+
+        if(decision.isDenied() && decision.reason.isRateLimit()){
+            logger.warn('Rate limit exceeded', { ip: req.ip, userAgent:req.get('User-Agent'), path:req.path, method:req.method });
+            
+            return res.status(403).json({ error: 'Forbidden', message: 'Too many requests' });
+        }
+
+        next();
+
+
+    }
+    catch(e){
+        console.log('Security middleware error',e);
+        return res.status(500).json({ error: 'Internal Server Error', message: 'Security check failed' });
+    }
+}
